b6e07871e4a574c8709ec172deb39bab90519a14
rakan <Rakan.w10@outlook.com>
2023-10-22 17:10:24 +0300

    Initial Commit

---
+++ SwiftUI_Assignment_01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Assignment_01
+//
+//  Created by Rakan Alotaibi on 07/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Assignment_01/SwiftUI_Assignment_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Assignment_01App.swift
+//  SwiftUI_Assignment_01
+//
+//  Created by Rakan Alotaibi on 07/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Assignment_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
